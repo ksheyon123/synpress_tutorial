436f142ec2bd388f361cae16060f73c59b3923c7
KagnSeohyeon <kshyeon123@gmail.com>
2024-04-04 17:07:18 +0900

    after test

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -5,13 +5,12 @@ describe("Synpress Demo With GitLab CI/CD", () => {
     cy.get("#connect-btn").click();
     cy.acceptMetamaskAccess();
 
-    cy.wait(10000);
-
-    cy.get("#address").contains("0xe985ef3c939de33f979283d486743475a93e8eac");
+    // cy.get("#address").contains("0xe985ef3c939de33f979283d486743475a93e8eac");
     // cy.get("#connected").contains("YES");
 
     // cy.get("#disconnect-btn").click();
     // cy.get("#address").contains("??");
     // cy.get("#connected").contains("NO");
+    cy.wait(5000000);
   });
 });
