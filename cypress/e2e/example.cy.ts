4ef827f3eb94e1433c30768f0bfc5cb0f0bd5989
KagnSeohyeon <shkang@coinplug.com>
2024-04-04 10:29:57 +0900

    :bug:fix > Synpress applied

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -2,12 +2,12 @@ describe("Synpress Demo With GitLab CI/CD", () => {
   it("should connect to MetaMask and display wallet address", () => {
     cy.visit("http://localhost:3000/");
     cy.get("#address").contains("??");
-    cy.get("#connected").contains("NO");
-
     cy.get("#connect-btn").click();
     cy.acceptMetamaskAccess();
 
-    // cy.get("#address").contains("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    cy.wait(10000);
+
+    cy.get("#address").contains("0xe985ef3c939de33f979283d486743475a93e8eac");
     // cy.get("#connected").contains("YES");
 
     // cy.get("#disconnect-btn").click();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import { Inter } from "next/font/google";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const address = "";
+  const [web3Provider, setWeb3Provider] = useState<any>();
+  const [account, setAccount] = useState<string>("");
+  const getProvider = async () => {
+    const provider = await detectEthereumProvider();
+    setWeb3Provider(provider);
+  };
+
+  useEffect(() => {
+    getProvider();
+  }, []);
+
+  const connectTo = async () => {
+    console.log(web3Provider);
+    const accounts = await web3Provider // Or window.ethereum if you don't support EIP-6963.
+      .request({ method: "eth_requestAccounts" })
+      .catch((err: any) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error.
+          // If this happens, the user rejected the connection request.
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(err);
+        }
+      });
+    const account = accounts[0];
+    setAccount(account);
+  };
 
   return (
     <main>
@@ -12,10 +39,15 @@ export default function Home() {
           <button id="disconnect-btn" onClick={() => {}}>
             Disconnect
           </button>
-          <button id="connect-btn" onClick={() => {}}>
+          <button
+            id="connect-btn"
+            onClick={() => {
+              connectTo();
+            }}
+          >
             Connect
           </button>
-          <p id="address">Address: {address || "??"}</p>
+          <p id="address">Address: {account || "??"}</p>
           <p>
             Edit <code>cypress/e2e/example.cy.ts</code> and save
           </p>
